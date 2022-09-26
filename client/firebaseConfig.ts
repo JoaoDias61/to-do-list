26327ecaf8fef5d1a2b314053604906d5579f19a
JoaoDias61 <jpdias641@gmail.com>
2022-09-26 09:19:22 -0300

    deploy

---
+++ client/firebaseConfig.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCi-SkYcQetbWMWfUealG_YNFHbuSVZJUo",
+  authDomain: "todo-app-84df5.firebaseapp.com",
+  projectId: "todo-app-84df5",
+  storageBucket: "todo-app-84df5.appspot.com",
+  messagingSenderId: "1071431653515",
+  appId: "1:1071431653515:web:aeaa527c69116755491c8e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
